ea4d5c539b3a85cff1b270c9f92510b9d491be08
Tanner Boysun <Tanner Boysun>
2016-08-29 20:16:44 -0700

    Added files into the repository. Made few changes

---
+++ hw1/MakeHelloWorld.java
@@ -0,0 +1,14 @@
+public class MakeHelloWorld{
+	
+	public static void main(String[] args){
+	
+	System.out.println("public class HelloWorld{");
+	System.out.println("public static void main(String[] args){");
+	System.out.println("System.out.println(\"hello, world\");");
+	System.out.println("}");
+	System.out.println("}");
+		
+	
+	}
+
+}
