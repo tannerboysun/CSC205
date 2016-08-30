c8da64c58373b8999b57e837155da319576c3810
Tanner Boysun <Tanner Boysun>
2016-08-30 16:21:07 -0700

    Added formatting to main source java file hw1

--- hw1/MakeHelloWorld.java
+++ hw1/MakeHelloWorld.java
@@ -1,7 +1,18 @@
+/*
+* This java application prints out the code to generate a
+* hello world application.
+*
+* @creator Tanner Boysun
+* @generated 2016.8.29
+*/
+
+
 public class MakeHelloWorld{
 	
 	public static void main(String[] args){
 	
+		System.out.println("/*\n*This java application prints the phrase hello, world to the\n *standard output stream. This code was program generated.\n*\n *@creator Tanner Boysun\n* @generated 2016.8.29\n*/");
+		
 		System.out.println("public class HelloWorld{");
 		System.out.println("\tpublic static void main(String[] args){");
 		System.out.println("\t\tSystem.out.println(\"hello, world\");");
