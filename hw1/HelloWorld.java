670e07fe68b77472ec9b10815a27fa32446122ce
Tanner Boysun <Tanner Boysun>
2016-08-30 16:28:26 -0700

    Re-compiled and fixed formatting

--- hw1/HelloWorld.java
+++ hw1/HelloWorld.java
@@ -1,3 +1,10 @@
+/*
+* This java application prints the phrase hello, world to the
+* standard output stream. This code was program generated.
+*
+* @creator Tanner Boysun
+* @generated 2016.8.29
+*/
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("hello, world");
